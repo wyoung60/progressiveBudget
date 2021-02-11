908be398ba341d7423801a54754e686899310813
Wes Young <wesley.young@du.edu>
2021-02-11 12:33:19 -0700

    Offline transactions working

--- public/offlineStorage.js
+++ public/offlineStorage.js
@@ -1,30 +1,52 @@
 window.addEventListener("offline", () => {
-  let transactionNumber = 1;
-  const request = indexedDB.open("OfflineBudget");
-  request.onupgradeneeded = (event) => {
-    const db = event.target.result;
-    const objectStore = db.createObjectStore("offlineTransactions", {
-      keyPath: "id",
-    });
-    objectStore.createIndex("name", "name", { unique: false });
-    objectStore.createIndex("value", "value", { unique: false });
-    objectStore.transaction.oncomplete = (event) => {
-      const addButton = document.querySelector("#add-btn");
-      addButton.addEventListener("click", () => {
-        const transactionData = {};
-        transactionData.id = transactionNumber;
-        transactionData.name = document.querySelector("#t-name").value;
-        transactionData.value = document.querySelector("#t-amount").value;
-        const transaction = db
-          .transaction("offlineTransactions", "readwrite")
-          .objectStore("offlineTransactions");
-        transaction.add(transactionData);
-        transactionNumber++;
-      });
-    };
+  const deleteRequest = indexedDB.deleteDatabase("OfflineBudget");
+  deleteRequest.onsuccess = (event) => {
+    console.log("Deleted");
   };
 });
 
+function saveRecord(data) {
+  if (!navigator.onLine) {
+    const request = indexedDB.open("OfflineBudget", 1);
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      const objectStore = db.createObjectStore("offlineTransactions", {
+        autoIncrement: true,
+      });
+      objectStore.createIndex("name", "name", { unique: false });
+      objectStore.createIndex("value", "value", { unique: false });
+      objectStore.createIndex("date", "date", { unique: true });
+    };
+    request.onsuccess = (event) => {
+      const db = event.target.result;
+      const transaction = db.transaction(["offlineTransactions"], "readwrite");
+      const objectStore = transaction.objectStore("offlineTransactions");
+      const addRequest = objectStore.add(data);
+      addRequest.onsuccess = (event) => {};
+    };
+  }
+}
+
 window.addEventListener("online", () => {
-  const request = indexedDB.deleteDatabase("OfflineBudget");
+  location.reload();
+  const request = indexedDB.open("OfflineBudget");
+  request.onsuccess = (event) => {
+    const db = event.target.result;
+    const transaction = db.transaction(["offlineTransactions"], "readonly");
+    const objectStore = transaction.objectStore("offlineTransactions");
+    const pullRequest = objectStore.getAll();
+    pullRequest.onsuccess = (event) => {
+      const data = event.target.result;
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((res) => {
+        return res.json();
+      });
+    };
+  };
 });
