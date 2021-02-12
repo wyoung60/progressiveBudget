b502a5df270dc14ddd5c269d32ae6471bc81753f
Wes Young <wesley.young@du.edu>
2021-02-12 10:11:48 -0700

    Added service-worker and changed offlineStorage.js

--- public/offlineStorage.js
+++ public/offlineStorage.js
@@ -1,45 +1,44 @@
+let db;
+
+const request = indexedDB.open("OfflineBudget", 1);
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+  const objectStore = db.createObjectStore("offlineTransactions", {
+    autoIncrement: true,
+  });
+  objectStore.createIndex("name", "name", { unique: false });
+  objectStore.createIndex("value", "value", { unique: false });
+  objectStore.createIndex("date", "date", { unique: true });
+};
+request.onsuccess = (event) => {
+  db = event.target.result;
+};
+
 function saveRecord(data) {
-  if (!navigator.onLine) {
-    const request = indexedDB.open("OfflineBudget", 1);
-    request.onupgradeneeded = (event) => {
-      const db = event.target.result;
-      const objectStore = db.createObjectStore("offlineTransactions", {
-        autoIncrement: true,
-      });
-      objectStore.createIndex("name", "name", { unique: false });
-      objectStore.createIndex("value", "value", { unique: false });
-      objectStore.createIndex("date", "date", { unique: true });
-    };
-    request.onsuccess = (event) => {
-      const db = event.target.result;
-      const transaction = db.transaction(["offlineTransactions"], "readwrite");
-      const objectStore = transaction.objectStore("offlineTransactions");
-      const addRequest = objectStore.add(data);
-      addRequest.onsuccess = (event) => {};
-    };
-  }
+  console.log(db);
+  const transaction = db.transaction(["offlineTransactions"], "readwrite");
+  console.log(transaction);
+  const objectStore = transaction.objectStore("offlineTransactions");
+  const addRequest = objectStore.add(data);
 }
 
 window.addEventListener("online", () => {
-  const request = indexedDB.open("OfflineBudget");
-  request.onsuccess = (event) => {
-    const db = event.target.result;
-    const transaction = db.transaction(["offlineTransactions"], "readwrite");
-    const objectStore = transaction.objectStore("offlineTransactions");
-    const pullRequest = objectStore.getAll();
-    objectStore.clear();
-    pullRequest.onsuccess = (event) => {
-      const data = event.target.result;
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }).then((res) => {
-        res.json();
-      });
-    };
+  console.log(db);
+  const transaction = db.transaction(["offlineTransactions"], "readwrite");
+  const objectStore = transaction.objectStore("offlineTransactions");
+  const pullRequest = objectStore.getAll();
+  objectStore.clear();
+  pullRequest.onsuccess = (event) => {
+    const data = event.target.result;
+    fetch("/api/transaction/bulk", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      res.json();
+    });
   };
 });
---
+++ public/service-worker.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = "url-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/offlineStorage.js",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    );
+    return;
+  }
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheAllowList = ["url-cache-v1", "data-cache-v1"];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((item) => {
+          if (cacheAllowList.indexOf(item) === -1) {
+            return caches.delete(item);
+          }
+        })
+      );
+    })
+  );
+});
